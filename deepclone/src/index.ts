ec7f2f6d30f6b61a602b940a93a475578c6ccbc6
liu zhenbin <536543269@qq.com>
2020-12-28 14:40:35 +0800

    deepClone

--- deepclone/src/index.ts
+++ deepclone/src/index.ts
@@ -1,26 +1,32 @@
-function deepClone(source:any){
+type Dictionary= {[key:string]:any}
+function deepClone(source:any, cache=new Map()){
     if(source instanceof Object){
-        if(source instanceof Array){
-            const result:Array<any> = []
-            for(let key in source){
-                result[key] = deepClone(source[key])
-            }
-            return result
-        }else if(source instanceof Function){
-            const result:any = function (){
-                return source.apply(this, arguments)
-            }
-            for(let key in source){
-                result[key] = deepClone(source[key])
-            }
-            return result
+        if(cache.has(source)){
+            return cache.get(source)
         }else{
-            let result:{[key in typeof source]:any} = {}
-            for(let key in source){
-                result[key] = deepClone(source[key])
+            let result:Dictionary;
+            if(source instanceof Array){
+                result= []
+            }else if(source instanceof Function){
+                result = function (this:any){
+                    return source.apply(this,arguments)
+                }
+            }else if(source instanceof RegExp){
+                result = new RegExp(source.source, source.flags)
+            }else if(source instanceof Date){
+                result = new Date(source)
+            }else{
+                result = new Object()
+            }
+            cache.set(source,result)
+            for (let key in source){
+                if(source.hasOwnProperty(key)) {
+                    result[key] = deepClone(source[key],cache)
+                }
             }
             return result
         }
+
 
     }
     return source
--- deepclone/test/index.ts
+++ deepclone/test/index.ts
@@ -5,60 +5,156 @@ import deepClone from "../src";
 
 const assert = chai.assert;
 chai.use(sinonChai);
-describe('deepClone',()=>{
-    it('是一个函数',()=>{
+
+type Dictionary = { [key: string]: any }
+describe('deepClone', () => {
+    it('是一个函数', () => {
         assert.isFunction(deepClone)
     })
     it("能够复制基本类型", () => {
-        const n = 123;
-        const n2 = deepClone(n);
-        assert(n === n2);
-        const s = "123456";
-        const s2 = deepClone(s);
-        assert(s === s2);
-        const b = true;
-        const b2 = deepClone(b);
-        assert(b === b2);
-        const u = undefined;
-        const u2 = deepClone(u);
-        assert(u === u2);
-        const empty = null;
-        const empty2 = deepClone(empty);
-        assert(empty === empty2);
-        const sym = Symbol();
-        const sym2 = deepClone(sym);
-        assert(sym === sym2);
+        const n = 1;
+        const n2 = deepClone(n)
+        assert(n === n2)
+
+        const s = '112'
+        const s2 = deepClone(s)
+        assert(s === s2)
+
+        const b = true
+        const b2 = deepClone(b)
+        assert(b === b2)
+
+        const u = undefined
+        const u2 = deepClone(u)
+        assert(u === u2)
+
+        const empty = null
+        const empty2 = deepClone(empty)
+        assert(empty === empty2)
+
+        const sym = Symbol()
+        const sym2 = deepClone(sym)
+        assert(sym === sym2)
     });
-    describe("对象", () => {
-        it("能够复制普通对象", () => {
-            const a = {name: "方方", child: {name: "小方方"}};
-            const a2 = deepClone(a);
-            assert(a !== a2);
-            assert(a.name === a2.name);
-            assert(a.child !== a2.child);
-            assert(a.child.name === a2.child.name);
-        });
-        it("能够复制数组对象", () => {
-            const a = [[11, 12], [21, 22], [31, 32]];
-            const a2 = deepClone(a);
-            assert(a !== a2);
-            assert(a[0] !== a2[0]);
-            assert(a[1] !== a2[1]);
-            assert(a[2] !== a2[2]);
-            assert.deepEqual(a, a2);
-        });
-        it("能够复制函数", () => {
-            const a = function(x:number, y:number) {
-                return x + y;
-            };
-            a.xxx = { yyy: { zzz: 1 } };
-            const a2 = deepClone(a);
-            assert(a !== a2);
+    describe('对象', () => {
+        it('能复制普通对象', () => {
+            const a = {name: 'lzb', address: {zipCode: '000000'}}
+            const a2 = deepClone(a)
+            assert(a !== a2)
+            assert(a.name === a2.name)
+            assert(a.address !== a2.address)
+            assert(a.address.zipCode === a2.address.zipCode)
+        })
+        it('能复制数组对象', () => {
+            const a = [[11, 12], [21, 22], [31, 32]]
+            const a2 = deepClone(a)
+            assert(a2 !== a)
+            assert(a2[0] !== a[0])
+            assert(a2[1] !== a[1])
+            assert(a2[2] !== a[2])
+            assert.deepEqual(a, a2)
+        })
+        it('能复制函数', () => {
+            const a = function (a: number, b: number) {
+                return a + b
+            }
+            a.xxx = {yyy: {zzz: 1}}
+            const a2 = deepClone(a)
+            assert(a !== a2)
             assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
             assert(a.xxx.yyy !== a2.xxx.yyy);
             assert(a.xxx !== a2.xxx);
             assert(a(1, 2) === a2(1, 2));
+        })
+        it('环也能复制', () => {
+            const a: Dictionary = {name: 'lzb', address: {zipCode: '000000'}}
+            a.self = a
+            const a2 = deepClone(a)
+            assert(a !== a2)
+            assert(a.name === a2.name)
+            assert(a.address !== a2.address)
+            assert(a.address.zipCode === a2.address.zipCode)
+        })
+        it('复制正则表达式', () => {
+            const a = new RegExp("hi\\d+", "gi")
+            const a2 = deepClone(a)
+            assert(a !== a2)
+            assert(a.source === a2.source)
+            assert(a.flags === a2.flags)
+        })
+        it('复制日期', () => {
+            const a = new Date()
+            const a2 = deepClone(a)
+            assert(a !== a2)
+            assert(a.getTime() === a2.getTime())
+        })
+        it('自动跳过原型属性', () => {
+            const a = Object.create({name: 'a'})
+            a.xxx = {yyy: {zzz: 1}}
+            const a2 = deepClone(a)
+            assert(a !== a2)
+            assert.isFalse('name' in a2)
+            assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
+            assert(a.xxx.yyy !== a2.xxx.yyy);
+            assert(a.xxx !== a2.xxx);
+        })
+        it("很复杂的对象", () => {
+            const a = {
+                n: NaN,
+                n2: Infinity,
+                s: "",
+                bool: false,
+                null: null,
+                u: undefined,
+                sym: Symbol(),
+                o: {
+                    n: NaN,
+                    n2: Infinity,
+                    s: "",
+                    bool: false,
+                    null: null,
+                    u: undefined,
+                    sym: Symbol()
+                },
+                array: [
+                    {
+                        n: NaN,
+                        n2: Infinity,
+                        s: "",
+                        bool: false,
+                        null: null,
+                        u: undefined,
+                        sym: Symbol()
+                    }
+                ]
+            };
+            const a2 = deepClone(a);
+            assert(a !== a2);
+            assert.isNaN(a2.n);
+            assert(a.n2 === a2.n2);
+            assert(a.s === a2.s);
+            assert(a.bool === a2.bool);
+            assert(a.null === a2.null);
+            assert(a.u === a2.u);
+            assert(a.sym === a2.sym);
+            assert(a.o !== a2.o);
+            assert.isNaN(a2.o.n);
+            assert(a.o.n2 === a2.o.n2);
+            assert(a.o.s === a2.o.s);
+            assert(a.o.bool === a2.o.bool);
+            assert(a.o.null === a2.o.null);
+            assert(a.o.u === a2.o.u);
+            assert(a.o.sym === a2.o.sym);
+            assert(a.array !== a2.array);
+            assert(a.array[0] !== a2.array[0]);
+            assert.isNaN(a2.array[0].n);
+            assert(a.array[0].n2 === a2.array[0].n2);
+            assert(a.array[0].s === a2.array[0].s);
+            assert(a.array[0].bool === a2.array[0].bool);
+            assert(a.array[0].null === a2.array[0].null);
+            assert(a.array[0].u === a2.array[0].u);
+            assert(a.array[0].sym === a2.array[0].sym);
         });
-    });
+    })
 
 })
